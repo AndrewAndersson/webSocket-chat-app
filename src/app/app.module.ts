d3853b3934c2be0334bb468efc55cd68386aeab5
andrii <anderssona258@gmail.com>
2018-11-04 11:32:49 +0200

    init some components && ws && node server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { WebsocketModule } from './websocket';
+import { environment } from '../environments/environment';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    ReactiveFormsModule,
+    WebsocketModule.config({
+        url: environment.ws
+    })
+],
   providers: [],
   bootstrap: [AppComponent]
 })
